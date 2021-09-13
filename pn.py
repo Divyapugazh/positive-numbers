ed84f1eec394f12fbaa38d55ca2a273ec94dff72
Divyapugazh <90617998+Divyapugazh@users.noreply.github.com>
2021-09-13 19:07:11 +0530

    created pn.py
    this file prints positive numbers in the screen

---
+++ pn.py
@@ -0,0 +1,7 @@
+nums1= [12,-7,5,64,-14]
+new_nums1= list(filter(lambda x: x>0,nums1))
+print("positive numbers:",new_nums1)
+
+nums2= [12,14,-95,3]
+new_nums2= list(filter(lambda y: y>0,nums2))
+print("positive numbers:",new_nums2)
